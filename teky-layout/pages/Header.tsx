5ce8c50aee523f69f38e11999da7d0eab9df1ae4
Moises SantAnna <moisespsantanna@gmail.com>
2023-09-21 08:58:27 -0300

    Refactoring Header

--- teky-layout/pages/Header.tsx
+++ teky-layout/pages/Header.tsx
@@ -1,25 +1,80 @@
-import '../style/header.css';
+import '../style/Header.css';
 
 function Header() {
     return (
-        <div className="header">
-            <button><img src='https://teky.com.br/static/media/stripe-icons.cadbe5d5.svg'></img></button>
-            <a href="#default">
-                <img src='./logo_principal.svg' className='logo'></img>
-            </a>
-            <div className='busca'>
-                <form>
-                    <select className='categoriaSelect'></select>
-                    <input type='text' className='pesquisa' placeholder='Faça sua Pesquisa'></input>
-                    <button className='pesquisar'></button>
-                </form>
+        <div className='navbar'>
+            <div className="header">
+                <div className='direita'>
+                    <a href='#'><img src='./Menu.svg'></img></a>
+                    <a href="#default">
+                        <img src='./tekyWhite.svg' className='logo'></img>
+                    </a>
+                </div>
+                <div className='centro'>
+                    <div className='busca'>
+                        <form>
+                            <select className='categoriaSelect'></select>
+                            <input type='text' className='pesquisa' placeholder='Pesquisar'></input>
+                            <button type='submit' className='buttonBusca'>S</button>
+                        </form>
+                    </div>
+                    <div className='login'>
+                        <p>Olá <br />
+                        <a href='/' className='headerLink'>Faça seu login</a>
+                        </p>
+                    </div>
+                </div>
+                <div className="esquerda">
+                    <a className="linkheader" href="#home">Quem somos</a>
+                    <p> | </p>
+                    <a className="linkheader" href="#contact">Teky Pro</a>
+                    <p> | </p>
+                    <a className="linkheader" href="#about">Venda na Teky</a>
+                    <p> | </p>
+                        <a className="linkheader" href='#'>Pedidos</a>
+                        <a><img src='./carrinho.png' className='carrinho'></img></a>
+
+                </div>
             </div>
-            <p>Olá</p>
-            <br/><a href='/'>Faça seu login</a>
-            <div className="header-right">
-                <a className="active" href="#home">Home</a>
-                <a href="#contact">Contact</a>
-                <a href="#about">About</a>
+            <div className='categorias'>
+                <ul className='categoria'>
+                    <li>
+                        <a className='categoriaHeader' href='#'>Abrasivos</a>
+                    </li>
+                    <li>
+                        <a className='categoriaHeader' href='#'>Correias e Mangueiras</a>
+                        </li>
+                    <li>
+                        <a className='categoriaHeader' href='#'>Corte e Usinagem</a>
+                        </li>
+                    <li>
+                        <a className='categoriaHeader' href='#'>Elétrica e Telecom</a>
+                        </li>
+                    <li>
+                        <a className='categoriaHeader' href='#'>EPIs e EPCs</a>
+                        </li>
+                    <li>
+                        <a className='categoriaHeader' href='#'>Ferramentas</a>
+                        </li>
+                    <li>
+                        <a className='categoriaHeader' href='#'>Ficação e Vedação</a>
+                        </li>
+                    <li>
+                        <a className='categoriaHeader' href='#'>Hidráulicos e Pneumáticos</a>
+                        </li>
+                    <li>
+                        <a className='categoriaHeader' href='#'>Motores e Bombas</a>
+                        </li>
+                    <li>
+                        <a className='categoriaHeader' href='#'>Óleos, Fluídos e Solventes</a>
+                        </li>
+                    <li>
+                        <a className='categoriaHeader' href='#'>Outros</a>
+                        </li>
+                    <li>
+                        <a className='categoriaHeader' href='#'>Rolamentos</a>
+                    </li>
+                </ul>
             </div>
         </div>
     );
