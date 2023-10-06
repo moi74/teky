674314d096d2f4dc4ac8673987aab43feefa89aa
Moises SantAnna <moisespsantanna@gmail.com>
2023-10-06 14:39:05 -0300

    Atualização Footer (sass e responsividade)

--- teky-layout/src/componentes/Footer/Footer.tsx
+++ teky-layout/src/componentes/Footer/Footer.tsx
@@ -1,59 +1,54 @@
-import './Footer.css';
 
-function Footer() {
-    return (
+import './Footer.scss'
+
+function Footer2() {
+    return <>
         <div className='footer'>
-                <div className='footer__register'>
-                <center>
-                    <p className='register footer__text'><b>Cadastre-se e receba ofertas exclusivas</b></p>
-                    <form className='register__data'>
-                        <input className='register__data__input' type='text' placeholder='Digite seu nome'></input>
-                        <input className='register__data__input' type='text' placeholder='Digite seu email'></input>
-                        <button type='submit' className='register__data__button'> Enviar </button>
-                    </form>
-                </center>
+            <div className='footer__register'>
+                <h2>Cadastre-se e receba ofertas exclusivas</h2>
+                <form className='footer__register__form'>
+                    <input type='text' placeholder='Digite seu nome'></input>
+                    <input type='text' placeholder='Digite seu email'></input>
+                    <button type='submit'> Enviar </button>
+                </form>
             </div>
             <div className='footer__higher'>
-                <a href='#'><img src='./logo_principal.svg' className='img_logo'></img></a>
-                <div className='div_content'>
-                    <strong>Redes Sociais</strong>
+                <a href='#'><img src='./logo_principal.svg' className='imgLogo'></img></a>
+                <div className='footer__higher__contents'>
+                    <h4>Redes Sociais</h4>
                 </div>
-                <div className='div_content'>
-                    <strong>Antendimento</strong><br />
-                    <a href='#' className='footer__link'>Fale Conosco</a>
-                    <a href='#' className='footer__link'>Trocas e Devoluções</a>
-                    <a href='#' className='footer__link'>Vendas na Teky</a>
+                <div className='footer__higher__contents'>
+                    <h4>Antendimento</h4>
+                    <a href='#' className='footerLink'>Fale Conosco</a><br />
+                    <a href='#' className='footerLink'>Trocas e Devoluções</a><br />
+                    <a href='#' className='footerLink'>Vendas na Teky</a><br />
                 </div>
-                <div className='div_content'>
-                    <strong>Institucional</strong><br />
-                    <a href='#' className='footer__link'>Blog</a>
-                    <a href='#' className='footer__link'>Quem Somos</a>
-                    <a href='#' className='footer__link'>Política de Privacidade</a>
-                    <a href='#' className='footer__link'>Política de Retirada</a>
+                <div className='footer__higher__contents'>
+                    <h4>Institucional</h4>
+                    <a href='#' className='footerLink'>Blog</a><br />
+                    <a href='#' className='footerLink'>Quem Somos</a><br />
+                    <a href='#' className='footerLink'>Política de Privacidade</a><br />
+                    <a href='#' className='footerLink'>Política de Retirada</a><br />
                 </div>
-                <div className='div_content'>
-                    <strong>Fale Conosco</strong><br />
-                    <a href='#' className='footer__link'>+55 (51) 4042-2076</a>
-                    <a href='#' className='footer__link'>contato@teky.com.br</a>
+                <div className='footer__higher__contents'>
+                    <h4>Fale Conosco</h4>
+                    <a href='#' className='footerLink'>+55 (51) 4042-2076</a><br />
+                    <a href='#' className='footerLink'>contato@teky.com.br</a><br />
                 </div>
-                <div className='div_content'>                    
-                    <strong>Formas de Pagamento</strong><br />
+                <div className='footer__higher__contents'>
+                    <h4>Formas de Pagamento</h4>
                 </div>
-                <div className='div_content'>
-                    <strong>Formas de Pagamento</strong>
-                </div>
-                <div className='div_content'>
-                    <strong>Site Seguro</strong>
+                <div className='footer__higher__contents'>
+                    <h4>Site Seguro</h4>
                 </div>
             </div>
             <div className='footer__bottom'>
-                <p className='footer__text'>Teky Intermediação de Negócios em Suprimentos Empresarias LTDA | 22.193.309/0001-88</p>
-                <p className='footer__text'>Av. Voluntários da Pátria, 2035, sala D - Floresta, Porto Alegre - Rio Grande do Sul, 90230-011</p>
-                <p className='footer__text'>Formas de Pagamento: Cartão de crédito, Boleto e Pix</p>
+                <p>Teky Intermediação de Negócios em Suprimentos Empresarias LTDA | 22.193.309/0001-88</p>
+                <p>Av. Voluntários da Pátria, 2035, sala D - Floresta, Porto Alegre - Rio Grande do Sul, 90230-011</p>
+                <p>Formas de Pagamento: Cartão de crédito, Boleto e Pix</p>
             </div>
         </div>
-    );
-  }
-  
-  export default Footer;
-  
+    </>
+}
+
+export default Footer2;
